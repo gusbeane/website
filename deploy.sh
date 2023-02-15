05d4505276fb8268dddf7a80be2a6b320bdbd043
Gus Beane <angus.beane@cfa.harvard.edu>
2023-02-15 17:46:27 -0500

    rebuilding site Wed Feb 15 17:46:26 EST 2023

--- deploy.sh
+++ deploy.sh
@@ -28,13 +28,14 @@ git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
+#if [ $# -eq 1 ]
+#  then msg="$1"
+#fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+#git push origin master
+git push
 
 # Come Back up to the Project Root
 cd ..
